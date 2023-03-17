613ce4837a6b9a548930a208ff4c2011447a2a21
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-03-17 09:59:49 +0630

    change

--- src/Card.js
+++ src/Card.js
@@ -1,68 +1,12 @@
-
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-// import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import React from 'react';
-// import data from './data';
 
-function GroupExample () {
-    
-  return (
-    
-    <CardGroup>
-      <Card>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>cloth</Card.Title>
-          <Card.Text>
-            
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-        <a href=""><button>+Add To Cart</button></a>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>cloth</Card.Title>
-          <Card.Text>
-            $50
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-        <a href=""><button>+Add To Cart</button></a>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>cloth</Card.Title>
-          <Card.Text>
-           $40
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-        <a href=""><button>+Add To Cart</button></a>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>cloth</Card.Title>
-          <Card.Text>
-            $50
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-        <a href=""><button>+Add To Cart</button></a>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
-  );
-}
+import data from './data';
+const App = () => {
+  // {data.productData.map((item,index) =>{
+  //   return(
+  //     <Card title={item.title} desc={item.desc} price={item.price} key={index}/>
+  //   )
+  // })}
+};
 
-export default GroupExample;
+export default App;
