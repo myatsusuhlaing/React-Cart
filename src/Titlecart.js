1cf9e4ec0d7ccf9b5cc9f0c346571ac0079c87ef
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-03-19 21:27:41 +0630

    adding static bedge

--- src/Titlecart.js
+++ src/Titlecart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
 import { AiFillShopping } from "react-icons/ai";
 
 function ContainerExample() {
@@ -12,6 +13,7 @@ function ContainerExample() {
             <p class="alls">All Products
             <button className='btn shopping-cart-btn'>
                 <AiFillShopping size={24}/>
+                <Badge bg="secondary"  size={4}>9</Badge>
               </button>
             </p>
         </Col>
