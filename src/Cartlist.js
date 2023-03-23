e4d00ca00e8c74d1421dc4be034f28aae129a743
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-03-23 13:15:57 +0630

    adding empty cart condition

--- src/Cartlist.js
+++ src/Cartlist.js
@@ -1,37 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Table from 'react-bootstrap/Table';
 import Counter from './counter';
 import data from './data';
 import Titlecart from './Titlecart';
 
-
 function StripedRowExample()  {
-  
+  const [isItem,noItem] = useState(false);
   return (
     <>
     <Titlecart/>
-    <Table className='cartlist-tb' striped hover size="sm"> 
-      <thead>
+    {isItem ? 
+    <Table className='cartlist-tb' striped hover size="sm">
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Price</th>
+        <th>Qty</th>
+      </tr>
+    </thead>
+    <tbody>
+      {data.productData.map((d) => <>
         <tr>
-          <th>Item</th>
-          <th>Price</th>
-          <th>Qty</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.productData.map((d) => <>
-          <tr>
-          <td>{d.title}</td>
-          <td>{d.price}</td>
-          <td>
-            <Counter/>
-          </td>
-        </tr>
-        </>
-        )
-        }
-      </tbody>
-    </Table>
+        <td>{d.title}</td>
+        <td>{d.price}</td>
+        <td>
+          <Counter/>
+        </td>
+      </tr>
+      </>
+      )
+      }
+    </tbody>
+  </Table>:<div className='noitems'> No Item Choose </div>}
     </>
   );  
 }
